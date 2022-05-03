531d92292d93f6eb61a71edba2214ef4f0c8110c
ValRedstone <valdorner31@gmail.com>
2022-05-03 15:19:49 +0200

    Update

--- src/pages/About.js
+++ src/pages/About.js
@@ -22,26 +22,26 @@ const About = () => {
                 <Card text={`Que votre chien soit adulte ou qu’il s’agisse d’un chiot nous travaillons avec des chiens de tous âges. Découvrez notre École du petit poilu à partir de l’âge de 2 mois.
                 A partir de septembre 2018, tous les samedis ou dimanches matin de 10h30 à 12h, des séances autour du chiot avec des jeux et des agrés afin de jouer et se sociabiliser.
                 Plusieurs ateliers : 2 à 4 mois, 4 à 6 mois et 6 à 8 mois.`} />
-                </div>
+            </div>
 
-                <div className="cardRow">
-                    <Card text={`Je m’appelle Carole BRUNEAU et je suis passionnée par mon métier !
-                    Ma p’tite meute de Husky et mes borders me font vivre un bonheur au quotidien.
-                    Je suis  diplômée d’État, formée aux premiers secours canins :
-                    - Attestation Niveau 1 et Niveau 2,
-                    - Intervenant PECCRAM (Programme d’Education à la Connaissance du Chien et aux Risques d’Accident par Morsures),
-                    - et bientôt comportementaliste canin (en formation). Je suis également présidente de deux clubs de sports canins.
-                    Les Canirider’s Ariège-Pyrénées (sport mono chien) et du CPSTC (Club Pyrénéen des sports de Traineau à chiens),
-                    sport d’attelage d’ou ma spécialité dans le chien nordique.`} />
-
-                    <Card text={`Possédant plusieurs Huskys, 2 Borders Collies et 1 European Sledog au centre d’éducation,
-                    et pratiquant le sport de traineau à chiens, nous sommes des spécialistes du chien nordique.
-                    N’hésitez plus pour nous confier votre Husky, Malamute, Samoyède, Groenlandais ou l’Alaskan !`} />
-                </div>
-
-                <Map />
+            <div className="cardRow">
+                <Card text={`Je m’appelle Carole BRUNEAU et je suis passionnée par mon métier !
+                Ma p’tite meute de Husky et mes borders me font vivre un bonheur au quotidien.
+                Je suis  diplômée d’État, formée aux premiers secours canins :
+                - Attestation Niveau 1 et Niveau 2,
+                - Intervenant PECCRAM (Programme d’Education à la Connaissance du Chien et aux Risques d’Accident par Morsures),
+                - et bientôt comportementaliste canin (en formation). Je suis également présidente de deux clubs de sports canins.
+                Les Canirider’s Ariège-Pyrénées (sport mono chien) et du CPSTC (Club Pyrénéen des sports de Traineau à chiens),
+                sport d’attelage d’ou ma spécialité dans le chien nordique.`} />
+            
+                <Card text={`Possédant plusieurs Huskys, 2 Borders Collies et 1 European Sledog au centre d’éducation,
+                et pratiquant le sport de traineau à chiens, nous sommes des spécialistes du chien nordique.
+                N’hésitez plus pour nous confier votre Husky, Malamute, Samoyède, Groenlandais ou l’Alaskan !`} />
+            </div>
+            
+            <Map />
         </div>
-        );
-    };
+    );
+};
     
-    export default About;
+export default About;
